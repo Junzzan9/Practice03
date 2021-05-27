e6e2ff02e1130a115a0bb9712cc0c5b332db65ac
junzzan9 <ojs2681@gmail.com>
2021-05-28 01:36:52 +0900

    5/28

--- src/com/javaex/practice/Ex11.java
+++ src/com/javaex/practice/Ex11.java
@@ -9,12 +9,18 @@ public class Ex11 {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("숫자를 입력하세요.");
 		int n = sc.nextInt();
+		int c = n%2;
 		int sum=0;
-		while (true) {
-			if (n%2==0) {
-				
+		for (int x=1;x<=n;x++) {
+			if(c==0&&x%2==0) {
+				sum=sum+x;
 			}
-		} 
+			else if (c==1&&x%2==1) {
+				sum=sum+x;
+			}
+		}
+		System.out.println("결과값: "+sum);
+		sc.close();
+		
 	}
-
 }
